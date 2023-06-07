73c5c24ad90ee77c9aa66a320837e63bd6238259
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-06-07 14:45:35 -0300

    Criando Resource Group, Vnet e Subnet

--- mod1/main.tf
+++ mod1/main.tf
@@ -7,16 +7,32 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-# Create a resource group
 resource "azurerm_resource_group" "mtc-rg" {
-  name     = "mtc-resources"
+  name     = "mtc-resource"
   location = "East Us"
   tags = {
-    "ambiente" = "dev"
+    environment = "dev"
   }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
 }
