f330b449ba78a2fe7de3fd23f74dc3bfe0c7d259
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-06-14 21:03:07 -0300

    Criando ip publico

--- mod15/virtualmachinewin.tf
+++ mod15/virtualmachinewin.tf
@@ -20,3 +20,12 @@ resource "azurerm_subnet" "subnet" {
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["10.0.2.0/24"]
 }
+
+resource "azurerm_public_ip" "publicip" {
+  name = "vmwinserver-ippublic"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Dynamic"
+  idle_timeout_in_minutes = 30
+  domain_name_label = "vmwinservertf"
+}
